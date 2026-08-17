934fd7250323885677eb9256e32230f1a94f89a4
KoushikGIT7 <fcgtub@gmail.com>
2026-08-17 22:21:09 +0530

    Akash Chnaages

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.8mautbdlf04"
+    "revision": "0.873meiqq67"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
