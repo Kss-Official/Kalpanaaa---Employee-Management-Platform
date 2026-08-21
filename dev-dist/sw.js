811c05c061a1de067b59a7f400c09d9a4ceef242
KoushikGIT7 <fcgtub@gmail.com>
2026-08-21 19:00:18 +0530

    build: update dev service worker bundle

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.oj1kkeol74c"
+    "revision": "0.l7vkp52bk18"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
