ba91ab9cb202c2d8a8ccc98995253c7b92d46ec1
KoushikGIT7 <fcgtub@gmail.com>
2026-08-24 19:15:48 +0530

    quizzzz

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.afd538hhmhg"
+    "revision": "0.mmd12b43bhg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
